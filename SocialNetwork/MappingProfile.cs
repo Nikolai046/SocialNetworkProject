bf77321b0e01cc4ee8703a11b8afd598275b99e4
Nick A <nakursk@gmail.com>
2025-02-06 17:38:33 +0300

    коммит 5

--- SocialNetwork/MappingProfile.cs
+++ SocialNetwork/MappingProfile.cs
@@ -1,6 +1,18 @@
-﻿namespace SocialNetwork;
+﻿using AutoMapper;
+using SocialNetwork.Models.Entities;
+using SocialNetwork.ViewModels.Account;
 
-public class MappingProfile
+namespace SocialNetwork;
+
+public class MappingProfile : Profile
 {
-    
+    public MappingProfile()
+    {
+        CreateMap<RegisterViewModel, User>()
+            .ForMember(x => x.BirthDate, opt => opt.MapFrom(c => new DateTime((int)c.Year, (int)c.Month, (int)c.Date)))
+            .ForMember(x => x.Email, opt => opt.MapFrom(c => c.EmailReg))
+            .ForMember(x => x.UserName, opt => opt.MapFrom(c => c.Login));
+        CreateMap<LoginViewModel, User>();
+
+    }
 }
